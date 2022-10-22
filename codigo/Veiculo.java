c91ffdbedcc61f6008f1a1f305474a37923d8eb7
lucas <lucassr614@gmail.com>
2022-10-22 16:19:23 -0300

    reupdate classe veiculo

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,20 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public abstract class Veiculo {
+    protected double valorVeiculo;
     protected double capacidadeTanque;
+
     protected double autonomiaDiaria;
-    protected Rota rota;
+    protected ArrayList<Rota> listaRotas;
     protected double quilometragremMedia;
     protected double ipva;
     protected double seguro;
     protected double kmPercorridos;
 
-    protected abstract void calcularIpva();
+    protected abstract double calcularIpva();
 
-    protected abstract void calcularSeguro();
+    protected abstract double calcularSeguro();
 
-    protected abstract void calcularOutrosCustos();
+    protected abstract double calcularOutrosCustos();
 
-    public void incluirRota() {
-
+    public double calculaDistanciaTotal() {
+        double distanciaTotal = 0;
+        for (Rota rota : listaRotas) {
+            distanciaTotal += rota.getDistancia();
+        }
+        return distanciaTotal;
     }
+
+    public void incluirRota(Rota novaRota) {
+        listaRotas.add(novaRota);
+    }
+
 
 }
