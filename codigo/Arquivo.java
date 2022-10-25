05957a83f0939eba401319f673edd101d6584152
Matheus <matheusfontes1921@gmail.com>
2022-10-25 13:48:13 -0300

    iniciando main

---
+++ codigo/Arquivo.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+
+public class Arquivo {
+    public static String Read(String Caminho) {
+        String conteudo = "";
+        try {
+            FileReader arq = new FileReader(Caminho);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha = "";
+            try {
+                linha = lerArq.readLine();
+                while (linha != null) {
+                    conteudo += linha+"\r\n";
+                    linha = lerArq.readLine();
+                }
+                arq.close();
+            }
+            catch (IOException ex) {
+                conteudo = "Erro -> Impossível ler arquivo.";
+            }
+
+        } catch (FileNotFoundException ex) {
+            conteudo = "Erro -> Arquivo não encontrado.";
+        }
+        if (conteudo.contains("Erro")) {
+            return "";
+        } else {
+            return conteudo;
+        }
+    }
+
+    public static boolean Write (String arquivo, String txt) {
+        try {
+            FileWriter arq = new FileWriter(arquivo);
+            PrintWriter gravarArq = new PrintWriter(arq);
+            gravarArq.println(txt);
+            gravarArq.close();
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+}
--- codigo/Main.java
+++ codigo/Main.java
@@ -1,3 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int escolha = 1;
+        while (escolha!=0) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("------MENU------");
+            System.out.println("Escolha uma opção: ");
+            System.out.println("[1] -> Carregar um conjunto de veículos de um arquivo ");
+            System.out.println("[2] -> Salvar um conjunto de veículos de um arquivo");
+            System.out.println("[3] -> Incluir um novo veículo");
+            System.out.println("[4] -> Incluir rotas para um veículo");
+            System.out.println("[5] -> Localizar um veículo da frota");
+            System.out.println("[6] -> Imprimir um relatório do veículo com seus gastos até o momento");
+            System.out.println("[0] -> Sair do Menu");
+            escolha = teclado.nextInt();
+            switch (escolha) {
+                case 1:
+                    carregarArquivo();
+                    break;
+
+                case 2:
+
+            }
+        }
+    }
+
+    private static void carregarArquivo() {
+        String arquivo = "/Users/Matheus/Desktop/PUC 2022.2/LaboratorioModular/Veiculos.txt";
+        String txt = Arquivo.Read(arquivo);
+        if (txt.isEmpty()) {
+            System.out.println("Não existe nenhum veículo cadastrado");
+        } else {
+            System.out.println(txt);
+        }
+    }
 
 }
