b80f802ac660c6599f1292db6f790eb94ab765f3
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2022-12-03 11:22:07 -0300

    public fabrica

--- codigo/app/fabricas/FabricaVeiculo.java
+++ codigo/app/fabricas/FabricaVeiculo.java
@@ -4,23 +4,15 @@ import app.veiculo.*;
 
 public class FabricaVeiculo {
     /* Fábrica genérica que engloba a criação de um app.veiculo qualquer*/
-    static Veiculo criarVeiculo(String tipo, double preco, String placa) throws Exception {
+    public static void criarVeiculo(String tipo, double preco, String placa) throws Exception {
         switch (tipo) {
-            case "app.veiculo.Caminhao" -> {
-                return new Caminhao(preco, placa);
-            }
+            case "app.veiculo.Caminhao" -> new Caminhao(preco, placa);
 
-            case "app.veiculo.Carro" -> {
-                return new Carro(preco, placa);
-            }
+            case "app.veiculo.Carro" -> new Carro(preco, placa);
 
-            case "app.veiculo.Furgao" -> {
-                return new Furgao(preco, placa);
-            }
+            case "app.veiculo.Furgao" -> new Furgao(preco, placa);
 
-            case "app.veiculo.Van" -> {
-                return new Van(preco, placa);
-            }
+            case "app.veiculo.Van" -> new Van(preco, placa);
 
             default -> throw new Exception("Tipo inexistente");
 
